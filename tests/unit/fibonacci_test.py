b46f67fa6775c6be8ef5fbe29055870465f4ac92
ben42code <1077747+ben42code@users.noreply.github.com>
2025-05-07 18:11:53 +0200

    Unit test cleaning (#10)
    - renamed some test cases for clarity/consistency
    - be explicit about arrange/act/assert steps

--- tests/unit/fibonacci_test.py
+++ tests/unit/fibonacci_test.py
@@ -23,19 +23,25 @@ generalTestCases: list[TestCase] = [
 
 class Fibo_Test(unittest.TestCase):
 
-    def test_callWithValidInput_0(self):
+    def test_callWith0_getExpectedresult(self):
+        # Arrange/Act
         result = fibonacci.fibo(0)
+        # Assert
         self.assertEqual(result, 0)
 
-    def test_callWithValidInput_6(self):
+    def test_callWith6_getExpectedresult(self):
+        # Arrange/Act
         result = fibonacci.fibo(6)
+        # Assert
         self.assertEqual(result, 8)
 
-    def test_callWithInvalidInput_negative(self):
+    def test_callWithNegativeInput_getAsserts(self):
+        # Assert
         with self.assertRaises(AssertionError):
+            # Arrange/Act
             fibonacci.fibo(-1)
 
-    def test_callWithValidInputsResultsAndComplexity(self):
+    def test_callWithValidInputs_getExpectedResultsAndComplexity(self):
 
         for testCase in generalTestCases:
             with self.subTest(testCase.n):
@@ -56,19 +62,25 @@ class Fibo_Test(unittest.TestCase):
 
 class FiboAsync_Test(unittest.IsolatedAsyncioTestCase):
 
-    async def test_callWithValidInput_0(self):
+    async def test_callWith0_getExpectedresult(self):
+        # Arrange/Act
         result = await fibonacci.fibo_async(0)
+        # Assert
         self.assertEqual(result, 0)
 
-    async def test_callWithValidInput_6(self):
+    async def test_callWith6_getExpectedresult(self):
+        # Arrange/Act
         result = await fibonacci.fibo_async(6)
+        # Assert
         self.assertEqual(result, 8)
 
-    async def test_callWithInvalidInput_negative(self):
+    async def test_callWithNegativeInput_getAsserts(self):
+        # Assert
         with self.assertRaises(AssertionError):
+            # Arrange/Act
             await fibonacci.fibo_async(-1)
 
-    async def test_callWithValidInputsResultsAndComplexity(self):
+    async def test_callWithValidInputs_getExpectedResultsAndComplexity(self):
 
         for testCase in generalTestCases:
             with self.subTest(testCase.n):
